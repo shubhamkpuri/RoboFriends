43ad362332441adf298ce7454df39b1199c8bcee
Shubham Kumar Puri <shubhampuri8169@gmail.com>
2018-07-20 12:27:51 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React,{Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots.js';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 
 class App extends Component{
     constructor(){
         super();
         this.state ={
-            robots : robots,
+            robots : [],
             searchfield:''
         }
+
     }
     onSearchChange = (event) => {
         this.setState({ searchfield :event.target.value});
-
-        // console.log(filteredRobots);
-
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots:users}) )
     }
     render (){
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-        <div className="text-center">
+        if(this.state.robots.length === 0){
+            return (
+                <div className="text-center">
 
-            <h1 className="mt-5"> Robo Friends </h1>
-            <SearchBox  searchChange={this.onSearchChange}/>
-            <hr/>
-            <CardList robots={filteredRobots} />
-        </div>
-    );
+                    <h1 className="mt-5"> Robo Friends </h1>
+                    <SearchBox  searchChange={this.onSearchChange}/>
+                    <hr/>
+                     <h1 className="text-center"> Loading... </h1>
+                </div>
+            );
+        }else{
+            return (
+                <div className="text-center">
+
+                    <h1 className="mt-5"> Robo Friends </h1>
+                    <SearchBox  searchChange={this.onSearchChange}/>
+                    <hr/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 
 }
